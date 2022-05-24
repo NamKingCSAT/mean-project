2f43f4167cba4717e108eb7e738d8a6721cda1c2
Nam King <namdocsat@gmail.com>
2022-05-24 10:04:08 -0400

    add Post interface/model

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 
+import { Post } from '../post.model';
+
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
@@ -12,5 +14,5 @@ export class PostListComponent {
     { title: 'Second Post', content: 'this is the second post content' },
     { title: 'Third Post', content: 'this is the third post content' },
   ];*/
-  @Input() posts = [];
+  @Input() posts: Post[] = [];
 }
