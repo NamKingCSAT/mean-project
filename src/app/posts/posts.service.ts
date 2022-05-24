3f6cc0ab9e5aed7fb06f18115cf6f3e0c48d6faf
Nam King <namdocsat@gmail.com>
2022-05-24 14:49:45 -0400

    Calling get post from service with the use of Observables

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,17 +1,25 @@
 import { Injectable } from '@angular/core';
 import { Post } from './post.model';
+import { Subject } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class PostsService {
   private posts: Post[] = [];
+  private postsUpdated = new Subject<Post[]>();
+
   getPosts() {
     //create a REAL copy of the object (not reference)
     //by pulling everything in the array and put into a new array
     return [...this.posts];
   }
 
+  getPostUpdateListener() {
+    return this.postsUpdated.asObservable();
+  }
+
   addPost(title: string, content: string) {
     const post: Post = { title: title, content: content };
     this.posts.push(post);
+    this.postsUpdated.next([...this.posts]);
   }
 }
